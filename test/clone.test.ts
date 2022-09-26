b60ba84d2668bb21a0189de2589c2615816c99e8
LoTwT <709937065@qq.com>
2022-09-26 16:32:54 +0800

    test: add command clone test cases

---
+++ test/clone.test.ts
@@ -0,0 +1,31 @@
+import { ensureDotGit, validateRepo } from "@/libs/clone"
+
+describe("command clone", () => {
+  test("validateRepo", () => {
+    expect(validateRepo("user/repo")).toBe(true)
+
+    expect(validateRepo("a-b/c-d")).toBe(true)
+    expect(validateRepo("a-b/c--d")).toBe(true)
+    expect(validateRepo("a-b/-c--d-")).toBe(true)
+    expect(validateRepo("a-b/--c--d--")).toBe(true)
+
+    expect(validateRepo("a-b/c_d")).toBe(true)
+    expect(validateRepo("a-b/c__d")).toBe(true)
+    expect(validateRepo("a-b/_c__d_")).toBe(true)
+    expect(validateRepo("a-b/__c__d__")).toBe(true)
+
+    expect(validateRepo("a-b/c.d")).toBe(true)
+    expect(validateRepo("a-b/c..d")).toBe(true)
+    expect(validateRepo("a-b/.c..d.")).toBe(true)
+    expect(validateRepo("a-b/..c..d..")).toBe(true)
+  })
+
+  test("ensureDotGit", () => {
+    expect(ensureDotGit("https://github.com/lotwt/venti")).toBe(
+      "https://github.com/lotwt/venti.git",
+    )
+    expect(ensureDotGit("https://github.com/lotwt/venti.git")).toBe(
+      "https://github.com/lotwt/venti.git",
+    )
+  })
+})
